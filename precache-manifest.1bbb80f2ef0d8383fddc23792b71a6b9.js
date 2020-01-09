7fd62b380f844bbd48579cabfd4b945962c8ea7b
Ivan.Moskvin <i.moskvin@modulbank.ru>
2020-01-09 15:14:36 +0700

    Updates

---
+++ precache-manifest.1bbb80f2ef0d8383fddc23792b71a6b9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fc2c9680613a92a129f785e691c811",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "c0b93ae8082eead677d8",
+    "url": "/minesweeper/static/css/main.82f8f367.chunk.css"
+  },
+  {
+    "revision": "f4396cfd5a9abb63e178",
+    "url": "/minesweeper/static/js/2.f0228156.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/minesweeper/static/js/2.f0228156.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0b93ae8082eead677d8",
+    "url": "/minesweeper/static/js/main.c8a44969.chunk.js"
+  },
+  {
+    "revision": "2340e72e9c17f94e04e5",
+    "url": "/minesweeper/static/js/runtime-main.7d9b8856.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/minesweeper/static/media/digit.58045dab.ttf"
+  },
+  {
+    "revision": "41c1c6c06d882e16e98bf7cd149834ca",
+    "url": "/minesweeper/static/media/flag-wrong.41c1c6c0.png"
+  },
+  {
+    "revision": "f1ff3e780ca5afebc57789fdede984f5",
+    "url": "/minesweeper/static/media/flag.f1ff3e78.png"
+  },
+  {
+    "revision": "4b6f3fc221f67fee9bb188c2e210c12d",
+    "url": "/minesweeper/static/media/mine.4b6f3fc2.png"
+  },
+  {
+    "revision": "052a10f7c73504f36a74fc7f6ffec163",
+    "url": "/minesweeper/static/media/mine3.052a10f7.png"
+  },
+  {
+    "revision": "14539aa40228735324bd68ad68f4a3bd",
+    "url": "/minesweeper/static/media/smiley.14539aa4.png"
+  },
+  {
+    "revision": "739a439c5a10c98a19c0e2974a037d59",
+    "url": "/minesweeper/static/media/smiley1.739a439c.png"
+  },
+  {
+    "revision": "ffec7fd1cc45f82d264921da6a147297",
+    "url": "/minesweeper/static/media/smiley2.ffec7fd1.png"
+  },
+  {
+    "revision": "e179ddace4f45cc2ad8cf73ade56b429",
+    "url": "/minesweeper/static/media/smiley3.e179ddac.png"
+  }
+]);
