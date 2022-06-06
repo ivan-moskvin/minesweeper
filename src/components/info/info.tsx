563b1c432b639bf1392663e2e49f03c6bb292ea6
Ivan Moskvin <glhf@vk.com>
2022-06-06 12:13:04 +0700

    feat: add repo link

--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -9,6 +9,9 @@ const Info = () => <div className='info'>
   <div className='info__row'>
     <strong>Shift + </strong><span className='info__lkm' /><span className='info__desc'> — {LANG.OPEN_NUMBER}</span>
   </div>
+  <div className='info__row'>
+    <a href="https://github.com/ivan-moskvin/minesweeper" target="_blank">Source code</a>
+  </div>
 </div>;
 
 export default Info;
